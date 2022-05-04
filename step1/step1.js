e20c13b0ea43f3ea98d56f658b784ca3460df032
gunami <58508367+gunami4573@users.noreply.github.com>
2022-05-04 11:28:40 +0900

    Step1 - 장면생성하기

--- step1/step1.js
+++ step1/step1.js
@@ -4,18 +4,15 @@ console.log(THREE);
 window.onload = function(){
     const step1 = document.querySelector('div.step1');
 
+    /* STEP1 */
     //장면생성
     const scene = new THREE.Scene();
-
-    //카메라세팅 PerspectiveCamera(시야, 종횡비, 근거리, 원거리)
+    //카메라세팅 PerspectiveCamera( 시야(fov), 종횡비, 근거리(near), 원거리(far) )
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
     //렌더러 변수생성
     const renderer = new THREE.WebGLRenderer();
-
     //렌더러 사이즈
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     //지정된 DOM 요소에 canvas DOM 생성
     step1.appendChild( renderer.domElement );
 }
